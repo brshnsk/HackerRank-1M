b7942c616910e256fe3cd9aa945a5b483758a535
brshnsk <brshnsk@gmail.com>
2023-03-11 14:22:12 -0400

    [DONE] Exercise 6

---
+++ Ejercicio6.ts
@@ -0,0 +1,8 @@
+/*You will be given a list of 32 bit unsigned integers. 
+Flip all the bits ( and ) and return the result as an unsigned integer. */
+
+
+function flippingBits(n: number): number {
+    
+    return ~n >>> 0
+}
